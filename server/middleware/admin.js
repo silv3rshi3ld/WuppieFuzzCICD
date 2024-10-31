ba72c03d11210781ad5d85463f0eb61c78966389
MBtje <silv3rshi3ld@gmail.com>
2024-10-31 10:47:32 +0100

    Delete server directory

--- server/middleware/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(req, res, next){
-    if(req.user.payload.role !== 'ADMIN') return res.status(403).send('Access Forbidden!');
-
-    next();
-}
--- server/startup/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const winston = require('winston');
-const config = require('config');
-
-module.exports = function(){
-    const db = process.env.DB_URL || config.get('db');
-    mongoose.connect(db)
-    .then(()=>{winston.info(`Connected to ${db}`)});
-}
