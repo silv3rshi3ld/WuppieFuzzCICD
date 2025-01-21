ba9068a6b87a926f429518d0b1b2f7bcc5a739d8
MBtje <michiel.brouwer@tno.nl>
2025-01-21 13:35:28 +0100

    Refactor entrypoint.sh to use new command line options for EvoMaster

--- services/evomaster/entrypoint.sh
+++ services/evomaster/entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 
 java -jar evomaster.jar \
-    -p "$SPEC_PATH" \
-    -o "$OUTPUT_DIR" \
-    --target_url="$TARGET_URL" \
-    --time_budget="$TIME_BUDGET"
+    --bbSwaggerUrl="$SPEC_PATH" \
+    --outputFolder="$OUTPUT_DIR" \
+    --bbTargetUrl="$TARGET_URL" \
+    --maxTime="$TIME_BUDGET" \
+    --blackBox=true
