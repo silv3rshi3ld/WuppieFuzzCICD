a2c9871b9878e7b0c03d86b76fd39550910d8390
silv3rshi3ld <silv3rshi3ld@gmail.com>
2025-04-27 18:09:43 +0200

    Change of count total requests for Restler parser based on the request summery

--- dashboard/data/restler/coverage.js
+++ dashboard/data/restler/coverage.js
@@ -1,34 +1,28 @@
 window.Restlercoverage = {
   "status_distribution": {
-    "hits": 4,
+    "hits": 3,
     "misses": 10,
     "unspecified": 0
   },
   "method_coverage": {
     "GET": {
-      "hits": 2,
+      "hits": 3,
       "misses": 10,
-      "unspecified": 0
-    },
-    "POST": {
-      "hits": 2,
-      "misses": 0,
       "unspecified": 0
     }
   },
   "status_codes": [
     500,
-    500,
-    500,
-    200,
-    200,
-    500,
-    500,
     200,
     500,
     500,
     500,
+    500,
     200,
+    500,
+    500,
+    200,
+    500,
     500,
     500
   ]
