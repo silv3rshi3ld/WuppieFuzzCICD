cd3b7cba6b2880fd5513b18d4002bf476e4e80bb
Michiel <mhetbrouwertje@gmail.com>
2025-04-29 12:17:58 +0200

    Add a comment at the end of app.py for clarification

--- services/vampi/app.py
+++ services/vampi/app.py
@@ -15,3 +15,4 @@ alive = int(os.getenv('tokentimetolive', 60))
 # start the app with port 5000 and debug on!
 if __name__ == '__main__':
     vuln_app.run(host='0.0.0.0', port=5000, debug=True)
+#comment
